a46ca1655668460a7e65e95d1f1d1873f5675925
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2025-08-28 09:59:50 -0400

    update curation code
    had two versions of heuristic & conversion script, bc first version of heuristic didn't accurately convert all of them

---
+++ curation/02_heudiconv_conversion/convert_all_heudiconv_reconvert.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --job-name=heudiconv_conversion_reconvert 
+#SBATCH --array=0-20   
+#SBATCH --time=24:00:00                  
+#SBATCH --cpus-per-task=4                
+#SBATCH --mem=8G                         
+#SBATCH --output=/cbica/projects/executive_function/code/heudiconv/logs/heudiconv_%A_%a.out # Output log
+#SBATCH --error=/cbica/projects/executive_function/code/heudiconv/logs/heudiconv_%A_%a.err  # Error log
+
+# Define paths
+base_dir="/cbica/projects/executive_function/data/bids/sourcedata/EFR01"
+output_dir="/cbica/projects/executive_function/data/bids/EF_bids_data_reconverted"
+
+# Step 1: Get all subject IDs
+subjects=("20124" "20125" "20141" "20236" "20333" "20335" "20336" "20347" "20350" "20305" "19861" "20139" "20214" "20259" "20410" "20253" "20399" "20188" "20252" "20352" "20149")
+
+# Step 2: Select the current subject based on the SLURM array task ID
+subID=${subjects[${SLURM_ARRAY_TASK_ID}]}
+
+
+# Step 3: Find all session directories for the current subject
+mapfile -t session_dirs < <(find ${base_dir}/SUBJECTS/${subID}/SESSIONS/* -maxdepth 0 -type d | sort -t '/' -k11n)
+
+
+# Step 4: Process each session directory with dynamic session renaming
+session_counter=1  # Initialize session counter
+for session_dir in "${session_dirs[@]}"; do
+    # Extract the numeric session name (e.g., 1153)
+    session=$(basename "$session_dir")
+
+    # Dynamically create the new session ID (e.g., ses-1, ses-2)
+    #new_session_id=$(printf "ses-%d" "$session_counter")
+    new_session_id="$session_counter"
+
+    echo "Processing session ${session} for subject ${subID} in directory ${session_dir} as ${new_session_id}"  # Debugging info
+
+
+    # Run heudiconv with dynamic session renaming
+    heudiconv \
+        -f /cbica/projects/executive_function/dropbox/heuristic_cubic_2_reconvert.py \
+        -o "$output_dir" \
+        --files $session_dir/ACQUISITIONS/*/FILES/*/*.dcm \
+        -s "$subID" \
+        -ss "$new_session_id" \
+        --bids 
+
+    # Increment session counter
+    session_counter=$((session_counter + 1))
+done
---
+++ curation/02_heudiconv_conversion/heuristic.py
@@ -0,0 +1,206 @@
+"""A HeuDiConv heuristic for the EF dataset."""
+
+from __future__ import annotations
+from typing import Optional
+
+from heudiconv.utils import SeqInfo
+
+
+def create_key(
+    template: Optional[str],
+    outtype: tuple[str, ...] = ("nii.gz",),
+    annotation_classes: None = None,
+) -> tuple[str, tuple[str, ...], None]:
+    if template is None or not template:
+        raise ValueError("Template must be a valid format string")
+    return (template, outtype, annotation_classes)
+
+
+def infotodict(seqinfo: list[SeqInfo]) -> dict[tuple[str, tuple[str, ...], None], list]:
+    """Heuristic evaluator for determining which runs belong where.
+
+    This function organizes the image sequence info and organizes it into a
+    structured dictionary that maps scan types to series ID based on meta-data.
+    Each key corresponds to a different type of image.
+    """
+    outdicom = ("dicom", "nii.gz")
+
+    # Define the different modalities with their BIDS keys
+    t1_norm = create_key(
+        "{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_rec-norm_T1w",
+        outtype=outdicom,
+    )
+    t2 = create_key(
+        "{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_T2w",
+        outtype=outdicom,
+    )
+    t2_norm = create_key(
+        "{bids_subject_session_dir}/anat/{bids_subject_session_prefix}_run-{item:02d}_rec-norm_T2w",
+        outtype=outdicom,
+    )
+    dwi = create_key(
+        "{bids_subject_session_dir}/dwi/{bids_subject_session_prefix}_run-{item:02d}_dwi",
+        outtype=outdicom,
+    )
+    asl = create_key(
+        "{bids_subject_session_dir}/perf/{bids_subject_session_prefix}_run-{item:02d}_asl",
+        outtype=outdicom,
+    )
+    asl_m0 = create_key(
+        "{bids_subject_session_dir}/perf/{bids_subject_session_prefix}_run-{item:02d}_m0scan",
+        outtype=outdicom,
+    )
+    qsm = create_key(
+        "{bids_subject_session_dir}/swi/{bids_subject_session_prefix}_run-{item:02d}_qsm",
+        outtype=outdicom,
+    )
+    func_nback = create_key(
+        "{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-nback_run-{item:02d}_bold",
+        outtype=outdicom,
+    )
+    func_rest = create_key(
+        "{bids_subject_session_dir}/func/{bids_subject_session_prefix}_task-rest_run-{item:02d}_bold",
+        outtype=outdicom,
+    )
+    dwi_fmap_ap = create_key(
+        "{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_acq-dwi_dir-AP_run-{item:02d}_epi",
+        outtype=outdicom,
+    )
+    dwi_fmap_pa = create_key(
+        "{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_acq-dwi_dir-PA_run-{item:02d}_epi",
+        outtype=outdicom,
+    )
+    fmri_fmap_ap = create_key(
+        "{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_acq-fmri_dir-AP_run-{item:02d}_epi",
+        outtype=outdicom,
+    )
+    fmri_fmap_pa = create_key(
+        "{bids_subject_session_dir}/fmap/{bids_subject_session_prefix}_acq-fmri_dir-PA_run-{item:02d}_epi",
+        outtype=outdicom,
+    )
+
+    # Dictionary to store the sequence info
+    # The keys in the dictionary are tuples, the values are each a list that
+    # holds the series IDs corresponding to each image modality.
+    info: dict[tuple[str, tuple[str, ...], None], list] = {
+        t1_norm: [],
+        t2: [],
+        t2_norm: [],
+        dwi_fmap_ap: [],
+        dwi_fmap_pa: [],
+        dwi: [],
+        fmri_fmap_ap: [],
+        fmri_fmap_pa: [],
+        func_nback: [],
+        func_rest: [],
+        asl: [],
+        asl_m0: [],
+        qsm: [],
+    }
+
+    # Loop through sequences and categorize them based on properties
+    for s in seqinfo:
+        # T1-weighted scans
+        if (
+            ("anat_t1w" in s.protocol_name) or
+            ("anat_T1w" in s.protocol_name) #or
+            #(s.protocol_name == "ABCD_T1w_MPR_vNav")
+            and (s.dim1 == 256)
+            and (s.dim2 == 256)
+            and ((s.dim3 == 352) or (s.dim3 == 176))
+        ):
+            # if not s.is_motion_corrected:
+            # assigning directly rather than appending updates the list with
+            # the most recent/correct scan
+            info[t1_norm].append(s.series_id)
+
+        # T2-weighted scans
+        elif (
+            ("anat_t2w" in s.protocol_name) or
+            ("anat_T2w" in s.protocol_name) #or
+            #(s.protocol_name == "ABCD_T2w_SPC_vNav")
+            and (s.dim1 == 256)
+            and (s.dim2 == 256)
+            and ((s.dim3 == 352) or (s.dim3 == 176))
+        ):
+            if "NORM" in s.image_type:
+                info[t2_norm].append(s.series_id)
+            else:
+                info[t2].append(s.series_id)
+
+        # Diffusion scans
+        elif (
+            (s.protocol_name == "dwi_acq-multishell_dir-AP_dwi") or
+            (s.protocol_name == "ABCD_dMRI")
+            and (s.dim1 == 140)
+            and (s.dim2 == 140)
+            and (s.dim3 == 81)
+        ):
+            info[dwi].append(s.series_id)
+
+        # distortion map, anterior to posterior
+        elif (
+            (s.protocol_name == "fmap_acq-dMRIdistmap_dir-AP_epi") or
+            (s.protocol_name == "ABCD_dMRI_DistortionMap_AP")
+        ):
+            info[dwi_fmap_ap].append(s.series_id)
+
+        # distortion map, posterior to anterior
+        elif (
+            (s.protocol_name == "fmap_acq-dMRIdistmap_dir-PA_epi") or
+            (s.protocol_name == "ABCD_dMRI_DistortionMap_PA")
+        ):
+            info[dwi_fmap_pa].append(s.series_id)
+
+        # Resting-state functional scans
+        elif (
+            ("rest" in s.protocol_name)
+            and (s.dim1 == 90)
+            and (s.dim2 == 90)
+            and (s.dim3 == 60)
+        ):
+            info[func_rest].append(s.series_id)
+
+        # N-back functional scans
+        elif (
+            ("fracnoback" in s.protocol_name) or
+            ("frac-no-back" in s.protocol_name)
+            and (s.dim1 == 90)
+            and (s.dim2 == 90)
+            and (s.dim3 == 60)
+        ):
+            info[func_nback].append(s.series_id)
+
+        # fMRI distortion maps
+        # distortion map, anterior to posterior
+        elif s.protocol_name in ("ABCD_fMRI_DistortionMap_AP", "fmap_acq-fMRIdistmap_dir-AP_epi"):
+            info[fmri_fmap_ap].append(s.series_id)
+
+        # distortion map, posterior to anterior
+        elif s.protocol_name in ("ABCD_fMRI_DistortionMap_PA", "fmap_acq-fMRIdistmap_dir-PA_epi"):
+            info[fmri_fmap_pa].append(s.series_id)
+
+        # ASL
+        elif (
+            (s.series_description == "asl_acq-3dspiralv20unbalanced_asl_ASL") or
+            (s.series_description == "ASL_3DSPIRAL_V20_GE_UnBalanced_ASL")
+        ):
+            info[asl].append(s.series_id)
+
+        # ASL M0 scan
+        elif (
+            (s.series_description == "asl_acq-3dspiralv20unbalanced_asl_M0") or
+            (s.series_description == "ASL_3DSPIRAL_V20_GE_UnBalanced_M0")
+        ):
+            info[asl_m0].append(s.series_id)
+
+        # QSM
+        elif (
+            (s.protocol_name == "qsm_acq-1.5mm_GRE") or
+            (('QSM_SWI' in s.protocol_name) and
+            (s.dim1 == 160) and
+            (s.dim2 == 120) and (s.dim3 == 384))
+        ):
+            info[qsm].append(s.series_id)
+
+    return info
